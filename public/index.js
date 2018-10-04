d848eb398a2b797f330de1e85605e9a87f2eb1a8
Edwin Lima <edwin.limaa@gmail.com>
2018-10-04 13:44:03 -0300

    Commit Inicial

---
+++ public/index.js
@@ -0,0 +1,63 @@
+window.onload = function() {
+    // verificando qual navegador esta em uso para usar o indexedDB específico
+    window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+    // verificando qual navegador esta em uso para usar a transação específica
+    window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction || {READ_WRITE: "readwrite"};
+
+    window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange;
+
+    //var formulario = document.getElementById('formulario');
+    //Botões
+    var addBtn = document.getElementById('addBtn');
+    var removeBtn = document.getElementById('removeBtn');
+    var getBtn = document.getElementById('getBtn');
+    var updateBtn = document.getElementById('updateBtn');
+    var form = document.getElementById('formulario');
+    /*Cardapio*/ 
+    var codigo = 0;  
+    var nome = document.getElementById('nome').value;
+    var preco = document.getElementById('preco').value;  
+
+    if (!window.indexedDB) {
+        window.alert("Seu Navegador não suporta versão estável do IndexedDB. \nOu seu Navegador está em modo de navegação anônimo. \n"+
+        "O recurso não está disponível.");
+    }else{
+            
+        form.addEventListener('submit', function(e){
+            e.preventDefault();
+            window.alert('Form Enviado');
+        });
+        
+    }
+    addBtn.addEventListener('click', function(){
+        console.log('Botao Adicionar');
+        //window.alert(document.getElementById('nome').value);
+        atualizaValorCampos();
+        console.log(nome);
+        console.log(preco);
+    });
+
+    removeBtn.addEventListener('click', function(){
+        console.log('Botao Remover');
+        atualizaValorCampos();
+        console.log(nome);
+        console.log(preco);
+    });
+
+    getBtn.addEventListener('click', function(){
+        console.log('Botao Localizar');        
+        atualizaValorCampos();
+        console.log(nome);
+        console.log(preco);
+    });
+    updateBtn.addEventListener('click', function(){
+        console.log('Botao Atualizar');
+        atualizaValorCampos();
+        console.log(nome);
+        console.log(preco);
+    });
+    function atualizaValorCampos(){
+        nome = document.getElementById('nome').value;
+        preco = document.getElementById('preco').value;
+    }
+}
---
+++ server.js
@@ -0,0 +1,38 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var express = require('express');
+var app = express();
+var port = 3000;
+app.use(express.static('public'));
+
+app.get('/', function(req, res){
+	res.render('index', { title: 'DWA Index DB', message: 'Hello there!' });
+});
+
+app.listen(port, function () {
+    console.log('Servidor Ativado, escutando porta '+ port);
+});
+/*http.createServer(function(request, response){
+	var q = url.parse(request.url, true);
+	var fileName = '.'+q.pathname;
+	console.log(fileName);
+	if(fileName == './'){
+		fileName = './public/index.html';
+	}
+	fs.readFile(fileName, function(err,data){
+		if(err){
+			console.log('deu ruim = '+err);
+			fs.readFile('404.html', function(erro, dados){
+				response.writeHead(200, {'contentType':'text/html;charset=utf-8'});
+				response.write(dados);
+				response.end();
+			});
+		}else{
+			response.writeHead(200, {'contentType':'text/html;charset=utf-8'});
+			response.write(data);
+			response.end();
+		}
+	});
+}).listen(3000);
+console.log('Servidor Ativado!!!!');*/
